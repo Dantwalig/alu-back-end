c09e762c6101617176bb6f76b4ece8a3569753a7
Dantwalig <d.gakumban@alustudent.com>
2025-11-07 18:51:05 +0200

    fix(pep validation)

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,19 +8,27 @@ import sys
 if __name__ == '__main__':
     user_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
         user_id)
 
-    user_info = requests.get(user_url).json()
-    todos_info = requests.get(todos_url).json()
+    user_response = requests.get(user_url)
+    todos_response = requests.get(todos_url)
+
+    user_info = user_response.json()
+    todos_info = todos_response.json()
 
     employee_name = user_info.get("name")
-    task_completed = [task for task in todos_info if task.get("completed")]
-    number_of_done_tasks = len(task_completed)
+    
+    completed_tasks = []
+    for task in todos_info:
+        if task.get("completed") is True:
+            completed_tasks.append(task)
+    
+    number_of_done_tasks = len(completed_tasks)
     total_number_of_tasks = len(todos_info)
 
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, number_of_done_tasks, total_number_of_tasks))
 
-    for task in task_completed:
+    for task in completed_tasks:
         print("\t {}".format(task.get("title")))
