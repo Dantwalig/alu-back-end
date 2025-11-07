78bd557cfd103529b3ca2e79e72954ac4cea281c
Dantwalig <d.gakumban@alustudent.com>
2025-11-07 18:57:39 +0200

    fix(gather data)

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,27 +8,24 @@ import sys
 if __name__ == '__main__':
     user_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
         user_id)
 
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    user_info = user_response.json()
-    todos_info = todos_response.json()
+    user_info = requests.get(user_url).json()
+    todos_info = requests.get(todos_url).json()
 
     employee_name = user_info.get("name")
+    number_of_done_tasks = 0
     
-    completed_tasks = []
     for task in todos_info:
         if task.get("completed") is True:
-            completed_tasks.append(task)
+            number_of_done_tasks += 1
     
-    number_of_done_tasks = len(completed_tasks)
     total_number_of_tasks = len(todos_info)
 
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, number_of_done_tasks, total_number_of_tasks))
 
-    for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
+    for task in todos_info:
+        if task.get("completed") is True:
+            print("\t {}".format(task.get("title")))
